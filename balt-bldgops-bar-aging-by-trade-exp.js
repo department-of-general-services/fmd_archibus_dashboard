25ba1aa5c16b41be2b584bd619d618785e46c004
James Trimarco <james.trimarco@baltimorecity.gov>
2020-11-18 14:12:44 -0500

    Placeholder for drilldown implmentation to come

--- balt-bldgops-bar-aging-by-trade-exp.js
+++ balt-bldgops-bar-aging-by-trade-exp.js
@@ -13,3 +13,22 @@ function onDrillDown(item) {
 
 }
 
+function test(item) {
+	var restriction = new Ab.view.Restriction();
+	var panel = View.panels.get("extDrillDownReportFrom2dChart_report");
+	panel.addParameter('summaryValueForThisGroup', item.selectedChartData['afm_cal_dates.month']);
+		
+	for(var cat in item.selectedChartData){
+		if(typeof cat != undefined && item.selectedChartData[cat] == item.selectedChartData.values.value){
+			panel.addParameter('restriction2Ctry', cat);
+			break;
+		}
+	}
+		
+	panel.refresh();
+    
+	panel.showInWindow({
+	            width: 300, 
+	            height: 200
+	        });
+}
