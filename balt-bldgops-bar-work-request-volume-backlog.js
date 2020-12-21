5eeaaa5f6eb3e1417363fcaf3088e21e343abf60
James Trimarco <james.trimarco@baltimorecity.gov>
2020-12-21 15:44:08 -0500

    second popup appears but is empty

--- balt-bldgops-bar-work-request-volume-backlog.js
+++ balt-bldgops-bar-work-request-volume-backlog.js
@@ -2,9 +2,9 @@ function onDrillDown(item) {
     //var restriction = new Ab.view.Restriction();
     //restriction.addClause('wrhwr.trade', item.selectedChartData['wrhwr.trade']);
     
-    var panel = View.panels.get("chartDrillDown_metrics");
+    var panel = View.panels.get("chartDrillDown_aging_by_trade");
 
-    panel.addParameter('summaryValueForThisGroup', item.selectedChartData['wrhwr.timespan']);
+    panel.addParameter('summaryValueForThisGroup', item.selectedChartData['wrhwr.trade']);
 
 	panel.refresh();
 
