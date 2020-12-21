2a0a450131746e23a3a7801496332f6d38add2b9
James Trimarco <james.trimarco@baltimorecity.gov>
2020-12-21 16:16:57 -0500

    consolidates .js functions

---
+++ balt-bldgops-dash-drilldown.js
@@ -0,0 +1,36 @@
+function onDrillDownByTrade(item) {
+	//var restriction = new Ab.view.Restriction();
+	//restriction.addClause('wrhwr.trade', item.selectedChartData['wrhwr.trade']);
+
+	var panel = View.panels.get("chartDrillDown_aging_by_trade");
+
+	panel.addParameter('summaryValueForThisGroup', item.selectedChartData['wrhwr.trade']);
+
+	panel.refresh();
+
+	panel.showInWindow({
+		width: 800,
+		height: 300
+	});
+
+}
+
+
+function onDrillDownByTimespan(item) {
+	//var restriction = new Ab.view.Restriction();
+	//restriction.addClause('wrhwr.trade', item.selectedChartData['wrhwr.trade']);
+
+	var panel = View.panels.get("chartDrillDown_metrics");
+
+	panel.addParameter('summaryTimespan', item.selectedChartData['wrhwr.timespan']);
+	panel.addParameter('summaryAction', item.selectedChartData['wrhwr.action']);
+
+	panel.refresh();
+
+	panel.showInWindow({
+		width: 800,
+		height: 300
+	});
+
+}
+
