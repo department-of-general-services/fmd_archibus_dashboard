25b46b97ebeb262fe073f860b0cded4637432a7d
James Trimarco <james.trimarco@gmail.com>
2021-04-29 11:15:13 -0400

    reformat only

--- create_views_facilities_dashboard.sql
+++ create_views_facilities_dashboard.sql
@@ -1,26 +1,27 @@
 USE [DGS_RAND_ARCHIBUS]
 GO
-	/****** Object:  View [afm].[dash_benchmarks]   Script Date: 1/11/2021 2:54:33 PM ******/
 SET
 	ANSI_NULLS ON
 GO
 SET
 	QUOTED_IDENTIFIER ON
 GO
-	DROP VIEW if exists [afm].[dash_cmu_problem_types]
+DROP VIEW if exists [afm].[dash_cmu_problem_types]
 GO
-	CREATE VIEW [afm].[dash_cmu_problem_types] AS (
-		SELECT
-			wr_id,
-			r.status,
-			r.description,
-			r.supervisor,
-			date_requested,
-			date_completed,
-			date_closed,
-			prob_type AS problem_type,
-			p.pm_group AS pm_group,
-			CASE
+CREATE VIEW [afm].[dash_cmu_problem_types]
+AS
+	(
+	SELECT
+		wr_id,
+		r.status,
+		r.description,
+		r.supervisor,
+		date_requested,
+		date_completed,
+		date_closed,
+		prob_type AS problem_type,
+		p.pm_group AS pm_group,
+		CASE
 				WHEN prob_type = 'AIR QUALITY' THEN 'AIR QUALITY'
 				WHEN prob_type = 'APPLIANCE' THEN 'APPLIANCE'
 				WHEN prob_type = 'BOILER' THEN 'HVAC'
@@ -36,12 +37,12 @@ GO
 				WHEN prob_type = 'DUCT CLEANING' THEN 'DUCT CLEANING'
 				WHEN (
 					prob_type LIKE 'ELEC%'
-					OR prob_type = 'OUTLETS'
+			OR prob_type = 'OUTLETS'
 				) THEN 'ELECTRICAL'
 				WHEN prob_type = 'ELEVATOR' THEN 'ELEVATOR'
 				WHEN (
 					prob_type LIKE 'ENVIR%'
-					OR prob_type = 'ASBESTOS'
+			OR prob_type = 'ASBESTOS'
 				) THEN 'ENVIRONMENTAL'
 				WHEN prob_type = 'FENCE_GATE' THEN 'FENCE GATE'
 				WHEN prob_type = 'FIRE SUPPRESSION-PROTECTION' THEN 'FIRE SUPPRESSION-PROTECTION'
@@ -61,9 +62,9 @@ GO
 					'INSPECTION',
 					'FUEL INSPECTION'
 				)
-				OR (
+			OR (
 					prob_type = 'PREVENTIVE MAINT'
-					AND p.pm_group IN (
+			AND p.pm_group IN (
 						'BASEMENT INSPECT',
 						'BLDG INSPECTION',
 						'ELEVATOR TEST',
@@ -77,9 +78,9 @@ GO
 					)
 				) THEN 'PREVENTIVE_GENERAL'
 				WHEN prob_type IN ('HVAC|PM', 'HVAC|INSPECTION')
-				OR (
+			OR (
 					prob_type = 'PREVENTIVE MAINT'
-					AND p.pm_group IN (
+			AND p.pm_group IN (
 						'HEAT CHECK TEST',
 						'HEATING LEVELS',
 						'HVAC INSPECTION',
@@ -92,11 +93,11 @@ GO
 				WHEN prob_type LIKE 'PLUMB%' THEN 'PLUMBING'
 				WHEN (
 					prob_type IN ('OTHER', 'RAMPS', 'STEPS', 'RAILSTAIRSRAMP')
-					AND u.role_name LIKE 'GATEKEEPER%'
+			AND u.role_name LIKE 'GATEKEEPER%'
 				) THEN 'OTHER-EXTERNAL'
 				WHEN (
 					prob_type IN ('OTHER', 'RAMPS', 'STEPS', 'RAILSTAIRSRAMP')
-					AND u.role_name NOT LIKE 'GATEKEEPER%'
+			AND u.role_name NOT LIKE 'GATEKEEPER%'
 				) THEN 'OTHER-INTERNAL'
 				WHEN prob_type = 'ROOF' THEN 'ROOF'
 				WHEN prob_type LIKE 'SECURITY SYSTEMS%' THEN 'SECURITY SYTEMS'
@@ -105,63 +106,65 @@ GO
 				WHEN prob_type = 'WINDOW' THEN 'WINDOW'
 				ELSE 'SMALL_TYPES_DISCARD'
 			END AS primary_type,
-			u.role_name,
-			b.name AS building_name,
-			b.bl_id AS b_number,
-			CASE
+		u.role_name,
+		b.name AS building_name,
+		b.bl_id AS b_number,
+		CASE
 				WHEN datepart(mm, date_requested) >= 7 THEN datepart(yy, date_requested) + 1
 				ELSE datepart(yy, date_requested)
 			END AS 'fy_request',
-			CASE
+		CASE
 				WHEN datepart(mm, date_closed) >= 7 THEN datepart(yy, date_closed) + 1
 				ELSE datepart(yy, date_closed)
 			END AS 'fy_close'
-		FROM
-			afm.wrhwr r
-			LEFT JOIN afm.afm_users u ON r.requestor = u.user_name
-			LEFT JOIN afm.bl b ON r.bl_id = b.bl_id
-			LEFT JOIN afm.pms p ON r.pms_id = p.pms_id
-		WHERE
-			prob_type IS NOT NULL --AND date_closed IS NOT NULL
-			AND prob_type != 'TEST (DO NOT USE)'
+	FROM
+		afm.wrhwr r
+		LEFT JOIN afm.afm_users u ON r.requestor = u.user_name
+		LEFT JOIN afm.bl b ON r.bl_id = b.bl_id
+		LEFT JOIN afm.pms p ON r.pms_id = p.pms_id
+	WHERE
+			prob_type IS NOT NULL
+		AND prob_type != 'TEST (DO NOT USE)'
 	);
 
 GO
-	DROP VIEW if exists [afm].[dash_benchmarks]
+DROP VIEW if exists [afm].[dash_benchmarks]
 GO
-	CREATE VIEW [afm].[dash_benchmarks] AS (
-		SELECT
-			wr_id,
-			status,
-			description,
-			supervisor,
-			date_completed,
-			date_requested,
-			date_closed,
-			fy_request,
-			role_name,
-			building_name,
-			b_number,
-			primary_type,
-			problem_type,
-			pm_group,
-			CONVERT(
+CREATE VIEW [afm].[dash_benchmarks]
+AS
+	(
+	SELECT
+		wr_id,
+		status,
+		description,
+		supervisor,
+		date_completed,
+		date_requested,
+		date_closed,
+		fy_request,
+		role_name,
+		building_name,
+		b_number,
+		primary_type,
+		problem_type,
+		pm_group,
+		CONVERT(
 				VARCHAR(7),
 				DateAdd(month, DateDiff(month, 0, date_requested), 0),
 				120
 			) AS calendar_month_request,
-			CONVERT(
+		CONVERT(
 				VARCHAR(7),
 				DateAdd(month, DateDiff(month, 0, date_closed), 0),
 				120
 			) AS calendar_month_close,
-			DATEDIFF(day, date_requested, date_completed) AS days_to_completion,
-			DATEDIFF(
+		DATEDIFF(day, date_requested, date_completed) AS days_to_completion,
+		DATEDIFF(
 				day,
 				date_requested,
 				DateAdd(year, - 2, getDate())
 			) AS days_open,
-			CASE
+		CASE
 				WHEN primary_type IN (
 					'BUILDING',
 					'DELIVERY',
@@ -200,8 +203,8 @@ GO
 				) THEN 45
 				WHEN primary_type IN ('DESIGN', 'ROOF', 'DUCT CLEANING') THEN 60
 			END AS benchmark
-		FROM
-			afm.dash_cmu_problem_types
+	FROM
+		afm.dash_cmu_problem_types
 	);
 
 GO
